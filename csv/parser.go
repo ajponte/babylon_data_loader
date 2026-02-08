f706abdcb4df7a10d7b32568f4f2d3bdf9dd9e84
Alan Ponte <alanjponte@gmail.com>
2026-02-07 19:31:14 -0800

    some refactoring

--- csv/parser.go
+++ csv/parser.go
@@ -4,5 +4,10 @@ import "context"
 
 // Parser defines the interface for parsing CSV data.
 type Parser interface {
-	Parse(ctx context.Context, filePath string, dataSource string, accountID string) ([]map[string]string, int64, error)
+	Parse(
+		ctx context.Context,
+		filePath string,
+		dataSource string,
+		accountID string,
+	) ([]map[string]string, int64, error)
 }
